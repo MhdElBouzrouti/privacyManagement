1aefd70a3c242783ef9a30c29aaa854a965716c0
Mohammed EL BOUZROUTI <Mohammedelbouzrouti@gmail.com>
2016-04-21 00:08:36 +0200

    fix bug

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # privacy Management
 ##### Privacy Management API REST
-a [Sails](http://sailsjs.org) application
- [![Build Status](https://travis-ci.com/MhdElBouzrouti/privacymanagement.svg?token=LXfLv9yfN3ybUqzBdNsZ&branch=master)](https://travis-ci.com/MhdElBouzrouti/privacymanagement)
+
+ [![Build Status](https://travis-ci.com/MhdElBouzrouti/privacyManagement.svg?token=LXfLv9yfN3ybUqzBdNsZ&branch=master)](https://travis-ci.com/MhdElBouzrouti/privacyManagement)
+[![Code Climate](https://codeclimate.com/repos/5717f8c065f2bc525f001a19/badges/105b6f290ac950cde28d/gpa.svg)](https://codeclimate.com/repos/5717f8c065f2bc525f001a19/feed)
--- api/controllers/EnforcePrivacyController.js
+++ api/controllers/EnforcePrivacyController.js
@@ -7,9 +7,13 @@
 
 module.exports = {
   'find': function (req, res) {
-    // Get variables
+    // Debug information
+
     sails.log.info('Variables');
     sails.log.debug(req.allParams());
+
+    // get all variables from url
+
     var privacyProfileId = req.allParams().privacyProfileId;
     var requestingPartyId = req.allParams().requestingPartyId;
     var name = req.allParams().name;
@@ -32,25 +36,41 @@ module.exports = {
     // Search privacy Profile
 
     PartyPrivacyProfile.findOne({id: privacyProfileId}).populateAll().exec(function (err, privacyProfile) {
+
       // verification of Result
-      if (err) {
+
+      if (err)
         return res.json(400, {error: 'This Profile is unavailable'});
-      }
+
       if (!privacyProfile)
         return res.json(400, {error: 'This Profile is unavailable'});
 
-      // Filter
+      // Find by privacyUsagePurpose and name
+
       var filter = _.find(privacyProfile.partyPrivacyProfileCharValue, {name: name, privacyUsagePurpose: privacyUsagePurpose});
+
+      // verification of result od search
       if(!filter)
-        return res.json(403,{message:'Party Privacy Characteristic (Name or Usage Purpose) is unavailable'})
+        return res.json(403,{message:'Party Privacy Characteristic (Name or Usage Purpose) does not march'});
+
+      // find related party
+
       var party = _.find(filter.relatedParty, {id: requestingPartyId});
+
+      // verification of result Party
+
       if(!party)
-        return res.json(400,{message:'Related Party'});
+        return res.json(400,{message:'Related Party does not march'});
+
+      // find the token
       StoreToken.findOne({privacyProfileId: privacyProfileId}).populateAll().exec(function (err, token) {
         sails.log.debug(token);
+
         var result = _.find(token.accessToken, {id: requestingPartyId});
+
         if(result.length)
           return res.json(403,{message:''});
+
         sails.log.debug(result);
         return res.json(200, {token: result.token});
       })
